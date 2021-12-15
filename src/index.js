7b09c3142453da781e4accad454281f136dcd258
linhuaxin <linhuaxin@foxmail.com>
2021-12-15 11:46:57 +0800

    feat: add enhancer

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ function createStore(reducer, state, enhancer) {
   let _state = state;
   let _listeners = [];
 
+  if (typeof enhancer === 'function') {
+    return enhancer(createStore, reducer, state);
+  }
+
   function getState() {
     return _state;
   }
 
   function dispatch(action) {
+    _state = reducer(_state, action);
+
     _listeners.forEach(listener => {
       listener();
     });
-
-    _state = reducer(_state, action);
   }
 
   function subscribe(listener) {
